3e85ea9e0f999b1e536a973c9a08f8ba72da18d2
Andy Sibilla <andy@liveaxle.com>
2017-12-04 10:44:39 -0800

    install dependencies, create required files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ENTRY = 'src/index.ts';
+const BUILD_DIR = 'dist';
+
+/**
+ * [exports description]
+ * @return {[type]} [description]
+ */
+module.exports = (env={}) => {
+
+  return {
+    entry: [path.join(__dirname, ENTRY)],
+    output: {
+      filename: "index.js",
+      path: path.join(__dirname, BUILD_DIR)
+    },
+    target: "web",
+
+    module: {
+      rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.ts?$/, loader: "awesome-typescript-loader" }
+        ]
+    },
+    resolve: {
+      alias: {
+        "~" : path.join(__dirname)
+      }
+    },
+    plugins: [
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost:8080/'
+      }),
+      new HtmlWebpackPlugin({
+        title: 'App Title',
+        template: path.join(__dirname, 'src/index.html'),
+      })
+    ]
+  };
+};
